e54c1ec79d6104384bd4524a3a9fbeef7b5e9d9e
JonnGXhaha <376419326@qq.com>
2019-07-23 23:44:59 +0800

    熔断练习

--- src/main/java/com/SpringbootTest1Application.java
+++ src/main/java/com/SpringbootTest1Application.java
@@ -2,17 +2,29 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEurekaClient
+//--------------------
+//@EnableEurekaClient
+@EnableDiscoveryClient
+//--------------------
 @EnableFeignClients
 @EnableTransactionManagement
+//熔断------------------
+@EnableCircuitBreaker
+//@EnableHystrix
+//----------------------
+
 public class SpringbootTest1Application {
 
 	@Bean
--- src/main/java/com/controller/springcloudTestController.java
+++ src/main/java/com/controller/springcloudTestController.java
@@ -1,12 +1,16 @@
 package com.controller;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import com.codingapi.tx.annotation.TxTransaction;
+//import com.codingapi.tx.annotation.TxTransaction;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class springcloudTestController {
@@ -23,14 +27,29 @@ public class springcloudTestController {
 	  *服务A 需要调用服务B,服务B 需要调用服务C，
 	  *此时 服务A为服务发起方，其余为参与方，参与方只需@Transaction 即可  
 	 * */
-	@TxTransaction(isStart = true)
+//	@TxTransaction(isStart = true)
+	/**
+	 * 熔断
+	 * */
+	@HystrixCommand(fallbackMethod="getPersonFallback")
 //	public Object getPerson(@PathVariable Long id) {
-		public Object getPerson() {
-		
+	public Object getPerson() {
+		Object userList_obj=restTemplate.getForObject("http://springBootTest-user/hello/getPersonList", Object.class);
 //		return restTemplate.getForObject("http://127.0.0.1:8086/hello/cacheDeleteOne?id="+id, Person.class);
 //		return restTemplate.getForObject("http://127.0.0.1:8086/hello/redisfindOne?id="+id, Person.class);
-		return restTemplate.getForObject("http://springBootTest-user/hello/getPersonList", Object.class);
+		Map<String, Object> map=new HashMap<>();
+		map.put("code", 0);
+		map.put("data", userList_obj);
+
+		return map;
 //		restTemplate.get
 //		return (List) restTemplate.getForObject("http://127.0.0.1:8086/hello/getPersonList", );
+	}
+	
+	public Object getPersonFallback() {
+		Map<String, Object> map=new HashMap<>();
+		map.put("code", -1);
+		map.put("msg", "当前访问数过大,请稍后再试.");
+		return map;
 	}
 }
