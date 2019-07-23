0205f182281a6b9a14f17be1e83d64339770808f
JonnGXhaha <376419326@qq.com>
2019-07-23 22:18:10 +0800

    测试提交

--- src/main/java/com/controller/springcloudTestController.java
+++ src/main/java/com/controller/springcloudTestController.java
@@ -21,7 +21,7 @@ public class springcloudTestController {
 	  *分布式事务 服务发起方
 	  *其中 @TxTransaction(isStart = true) 为lcn 事务控制注解，其中isStart = true 表示该方法是事务的发起方例如，
 	  *服务A 需要调用服务B,服务B 需要调用服务C，
-	  *此时 服务A为服务发起方，其余为参与方，参与方只需@Transaction 即可
+	  *此时 服务A为服务发起方，其余为参与方，参与方只需@Transaction 即可  
 	 * */
 	@TxTransaction(isStart = true)
 //	public Object getPerson(@PathVariable Long id) {
